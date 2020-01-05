1991879770b57aee79a705789728de3a3c02cfd1
RoccoDev <roccodev.business@gmail.com>
2020-01-05 18:19:04 +0100

    Remove 1.13 from master

--- 1.13.2/src/main/java/eu/the5zig/mod/mixin/MixinGameSettings.java
+++
@@ -1,8 +0,0 @@
-package eu.the5zig.mod.mixin;
-
-import net.minecraft.client.GameSettings;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(GameSettings.class)
-public class MixinGameSettings {
-}
