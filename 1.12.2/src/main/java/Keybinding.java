d86c46da0d1a4888e48a2a2d8d07c5c6d7e6b6f8
RoccoDev <roccodev.business@gmail.com>
2020-04-25 17:45:58 +0200

    Fix Controls menu crashing on 1.12.2 vanilla
    Fixes #39
    Closes #44

--- 1.12.2/src/main/java/Keybinding.java
+++ 1.12.2/src/main/java/Keybinding.java
@@ -16,8 +16,12 @@
  * along with The 5zig Mod.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import eu.the5zig.mod.asm.Transformer;
 import eu.the5zig.mod.util.IKeybinding;
 import net.minecraft.client.settings.KeyBinding;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 /**
  * Created by 5zig.
@@ -27,6 +31,7 @@ public class Keybinding extends KeyBinding implements IKeybinding {
 
     public Keybinding(String description, int keyCode, String category) {
         super(description, keyCode, category);
+        tryRegisterCategory(category);
     }
 
     @Override
@@ -41,5 +46,19 @@ public class Keybinding extends KeyBinding implements IKeybinding {
     @Override
     public int compareTo(KeyBinding o) {
         return super.compareTo(o);
+    }
+
+    private static void tryRegisterCategory(String category) {
+        Map<String, Integer> categories;
+        try {
+            Field field = Keybinding.class.getSuperclass().getDeclaredField(Transformer.FORGE ? "field_193627_d" : "d");
+            field.setAccessible(true);
+            categories = (Map<String, Integer>) field.get(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (!categories.containsKey(category)) {
+            categories.put(category, categories.size() + 1);
+        }
     }
 }
